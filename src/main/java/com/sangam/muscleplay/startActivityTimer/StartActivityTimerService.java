4cb0199660b8cc5f0c14c5584b5a17f402f5fb49
SangamGarg <sangamgarg17@gmail.com>
2024-08-08 13:24:41 +0530

    Build Error Solved...

--- src/main/java/com/sangam/muscleplay/startActivityTimer/StartActivityTimerService.java
+++
@@ -1,6 +0,0 @@
-package com.sangam.muscleplay.startActivityTimer;
-
-public interface StartActivityTimerService {
-
-    public String getActivityHistory();
-}
--- src/main/java/com/sangam/muscleplay/startActivityTimer/StartActivityTimerServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.sangam.muscleplay.startActivityTimer;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class StartActivityTimerServiceImpl {
-
-}
